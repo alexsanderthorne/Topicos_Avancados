c73c4e62a2aeeb9e68b79a72715df3206cf01384
alexsanderthorne <alexsanderthorne@gmail.com>
2021-08-22 20:20:02 -0300

    teste meclasses

---
+++ dog.py
@@ -0,0 +1,26 @@
+
+class Meta(type):
+    def __new__(self, class_name, bases, attrs):
+        print(attrs)
+
+        a = {}
+        for name, val in attrs.items():
+            if name.startswith("__"):
+                a[name] = val
+            else:
+                a[name.upper()] = val
+        print(a)
+        return type(class_name, bases, a)
+
+
+class Dog(metaclass=Meta):
+
+    x = 9
+    e = 3
+
+    def hello(self):
+        print("hello")
+
+d = Dog()
+print(d.X)
+print(d.HELLO())
--- intro.py
+++ intro.py
@@ -1,22 +1,24 @@
 from queue import Queue as q
 import inspect
 
+
 class Queue(q):
-    
+
     def __repr__(self):
         return f"Queue({self._qsize()})"
-    
-    def __add__(self,item):
+
+    def __add__(self, item):
         self.put(item)
-        
+
     def __sub__(self, item):
         self.get()
-        
+
+
 qu = Queue()
 
 qu + 7
 qu + 88
 qu + 1
-qu +8
+qu + 8
 
 print(qu)
