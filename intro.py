f9fa4ee0b66d4766b228e1e8477f61c0e390ad57
alexsanderthorne <alexsanderthorne@gmail.com>
2021-08-22 18:33:09 -0300

    data models

---
+++ intro.py
@@ -0,0 +1,22 @@
+from queue import Queue as q
+import inspect
+
+class Queue(q):
+    
+    def __repr__(self):
+        return f"Queue({self._qsize()})"
+    
+    def __add__(self,item):
+        self.put(item)
+        
+    def __sub__(self, item):
+        self.get()
+        
+qu = Queue()
+
+qu + 7
+qu + 88
+qu + 1
+qu +8
+
+print(qu)
