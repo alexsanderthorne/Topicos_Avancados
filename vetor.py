e8adeb2cfeee5e67bd86b6075275b3ea1093dea9
alexsanderthorne <alexsanderthorne@gmail.com>
2021-08-22 16:37:32 -0300

    add vetores

---
+++ vetor.py
@@ -0,0 +1,25 @@
+class Vetor:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vetor (%r,%r)' % (self.x, self.y)
+
+    def __mul__(self, escalar):
+        x = self.x * escalar
+        y = self.y *escalar
+        return Vetor(x,y)
+
+    def __add__(self, outro):
+        x = self.x + outro.x
+        y = self.y + outro.y
+        return Vetor(x,y)
+    
+v = Vetor(3,5)
+print(v*3)
+
+v1 = Vetor(9,4)
+v2 = Vetor(8,2)
+print(v1+v2)
