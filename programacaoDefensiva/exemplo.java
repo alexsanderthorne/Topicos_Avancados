534e2b2b6f50bec8922f116904d66ee4cfea9576
alexsanderthorne <alexsanderthorne@gmail.com>
2021-11-16 22:46:27 -0300

    programaçaoDefensiva

---
+++ programacaoDefensiva/exemplo.java
@@ -0,0 +1,33 @@
+import java.util.Optional;
+import java.util.Random;
+
+public class exemplo {
+
+    public static String getString() {
+        return "OK";
+    }
+
+    public static Optional<String> getString2() {
+
+        String retorno;
+
+        if (new Random().nextInt(2) == 0) {
+
+            retorno = null;
+
+        } else {
+            retorno = "OK";
+        }
+
+        return Optional.ofNullable(retorno);
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Optional<String> op = getString2();
+            if (op.isPresent()) {
+                System.out.println(op.orElse("NULL"));
+            }
+        }
+    }
+}
